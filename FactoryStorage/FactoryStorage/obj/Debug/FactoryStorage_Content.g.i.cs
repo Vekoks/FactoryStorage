de2391d5a104305fcf2f9b68fe6576d922685797
Vekoks <veselin.k.krustev@gmail.com>
2021-04-06 00:09:46 +0300

    Add image for background.

--- FactoryStorage/FactoryStorage/obj/Debug/FactoryStorage_Content.g.i.cs
+++ FactoryStorage/FactoryStorage/obj/Debug/FactoryStorage_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/transaction.txt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/picture/schemebackground.jpg")]
 
 
